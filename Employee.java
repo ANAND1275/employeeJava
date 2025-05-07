6dd58e731fe82068eef3b1c4bc2f46a7e3860552
ANAND1275 <2005anandpatil@gmail.com>
2025-05-07 15:04:14 +0530

    save to main

--- Employee.java
+++ Employee.java
@@ -2,17 +2,20 @@ public class Employee {
     String name;
     int id;
     double salary;
+    int age;
 
     public Employee(String name, int id, double salary) {
         this.name = name;
         this.id = id;
         this.salary = salary;
+        this.age = age;
     }
 
     public void displayInfo() {
         System.out.println("Employee ID :" + id);
         System.out.println("Employee Name :" + name);
         System.out.println("Employee Salary :" + salary);
+        System.out.println("Employee Age :" + age);
     }
 
     public static void main(String[] args) {
