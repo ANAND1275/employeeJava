45d8f19d1b28421c2b5988f79922b715ea46d877
ANAND1275 <2005anandpatil@gmail.com>
2025-05-07 14:38:58 +0530

    add Employee.java

---
+++ Employee.java
@@ -0,0 +1,22 @@
+public class Employee {
+    String name;
+    int id;
+    double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayInfo() {
+        System.out.println("Employee ID :" + id);
+        System.out.println("Employee Name :" + name);
+        System.out.println("Employee Salary :" + salary);
+    }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee("John", 1, 5000.0);
+        emp.displayInfo();
+    }
+}
